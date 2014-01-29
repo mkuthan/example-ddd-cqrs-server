7179373f302236289e8b3df553e1e9f635b2c169
Marcin Kuthan <marcin.kuthan@gmail.com>
2014-01-29 20:27:13 +0100

    Load data from sql file for query stack tests.

--- src/test/java/example/scrumboard/rest/queries/RestQueryTestConfig.java
+++ src/test/java/example/scrumboard/rest/queries/RestQueryTestConfig.java
@@ -8,15 +8,15 @@ import org.springframework.context.annotation.PropertySource;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
 
-import example.scrumboard.config.ScrumBoardModulesConfig;
+import example.scrumboard.infrastructure.rest.ScrumBoardInfrastructureRestConfig;
 
-@Import(ScrumBoardModulesConfig.class)
+@Import({ ScrumBoardInfrastructureRestConfig.class, ScrumBoardRestQueriesConfig.class })
 @PropertySource("classpath:/test.properties")
 public class RestQueryTestConfig {
 
 	@Bean
 	public DataSource dataSource() {
-		return new EmbeddedDatabaseBuilder().setType(EmbeddedDatabaseType.H2).build();
+		return new EmbeddedDatabaseBuilder().setType(EmbeddedDatabaseType.H2).addScript("sample.sql").build();
 	}
 
 }
